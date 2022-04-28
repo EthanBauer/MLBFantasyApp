5d5fe63cf2779235e885dcbcd97fcd68827a8358
Ethan Bauer <ethanbauer@MacBook-Pro.local>
2022-04-27 22:03:06 -0400

    newer

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ st.title('MLB Player Stats Explorer for Fantasy')
 st.markdown("""
 This app performs simple webscraping of MLB player stats data!
 * **Python libraries:** base64, pandas, streamlit
-* **Data source:** [Baseball-reference.com](https://www.baseball-reference.com/).
+* **Data source:** [FantasyPros.com](https://www.fantasypros.com/mlb/stats/hitters.php).
 """)
 html = pd.read_html("https://www.fantasypros.com/mlb/stats/hitters.php")
 df = html[0]
@@ -30,6 +30,10 @@ df.sort_values('zTotal', axis=0, ascending=False, inplace=True, na_position='fir
 df = df.reset_index(drop=True)
 
 st.header('Display Player Stats of All Teams in MLB')
+st.text(f"This table calculates and adds the zScore for each fantasy baseball category:\n   Runs ['R'], Home Runs ['HR'], Runs Batted In ['RBI'], Stolen Bases ['SB']\n     and Batting Average ['AVG']")
+st.text(f"There is an added z[category] column for each category.\nThis is the zScore of that specific category for that specific player.")
+st.text(f"Example: {df.loc[0][0]} has {df.loc[0][2]} Runs and his zScore for runs is {df.loc[0][16]}.\nA score of zR = 0 would mean that Anthony Rizzo is exactly average.\n    However, a positive zR in this case means that\n        {df.loc[0][0]} is that much above average.\nThe zTotal is the average of all fantasy categories (zR + zHR + zRBI + zSB + zAVG)/5")
+st.text(f"zScore for one category is calculated as:\n((specific players R)-(total R mean))/(total standard deviation of all players R)")
 st.write('Data Dimensions: ', df.shape[0], ' rows and ', df.shape[1], ' columns.')
 st.dataframe(df)
 def filedownload(df):
